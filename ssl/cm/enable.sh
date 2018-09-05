6c53731bb56c260f9ad4e43db30aef3c82c2f500
Eric Lin <ericlin@ericlin.me>
2018-09-05 11:56:40 +1000

    Added auto restart for Management services after CM enabled with SSL

--- ssl/cm/enable.sh
+++ ssl/cm/enable.sh
@@ -23,6 +23,8 @@ TLS_ENABLED=$2
 
 BASE_DIR=$(dirname $0)
 
+source $BASE_DIR/../../config.sh $CM_HOST $TLS_ENABLED
+
 # Update CM Configurations to enable TLS for CM, CM Management Services and CM Agents
 sh $BASE_DIR/update-cm-config.sh $CM_HOST $TLS_ENABLED
 
@@ -32,12 +34,28 @@ echo -n "And confirm \"yes\" to continue restarting CM server and agents: "
 read response
 
 if [ $response == "yes" ]; then
-  sh $BASE_DIR/enable-agent.sh $CM_HOST $TLS_ENABLED
+  #sh $BASE_DIR/enable-agent.sh $CM_HOST $TLS_ENABLED
 
   echo ""
   echo "Restarting CM server on host: $CM_HOST"
   ssh root@$CM_HOST 'service cloudera-scm-server restart'
   echo ""
-  echo "After CM restarted, please log into CM and restart Cloudera Management services"
+
+  # do not capture the error so that we can continue checking CM's status
+  set +e
+
+  # waiting for CM to start so that we can restart CM management services
+  test=""
+  echo "Waiting 10 seconds for CM to start up:"
+  while [ -z "$test" ]
+  do
+    sleep 5;
+    echo "Continue waiting 5 seconds for CM to start up:"
+    test=`curl -s -u $CM_USER:$CM_PASS "$CM_URL/api/version" $INSECURE`
+  done
+
   echo ""
+  echo "Restarting CM Management Services"
+  curl -s -S -X POST -H "Content-Type:application/json" -u $CM_USER:$CM_PASS $INSECURE \
+  "$API_URL/cm/service/commands/restart"
 fi
